8ae3d4ae4bb20de533d00eccc4e035cf289c8857
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-11-09 08:27:28 +0800

    feat: Implement updateProfile function for user profile updates

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,16 +1,19 @@
 import { Hono } from "hono";
 
+import { getAllClasses } from "../controllers/Classrooms.controller.js";
 import {
   getUserByEmail,
   getUserById,
+  updateProfile,
 } from "../controllers/Users.controller.js";
-import { getAllClasses } from "../controllers/Classrooms.controller.js";
 
 const app = new Hono();
 
 app.get("/", (c) => getUserByEmail(c));
 
 app.get("/:userId", (c) => getUserById(c));
+
+app.patch("/:userId", (c) => updateProfile(c));
 
 app.get("/:userId/classrooms", (c) => getAllClasses(c));
 
